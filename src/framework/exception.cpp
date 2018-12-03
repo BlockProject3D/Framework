b504e8214e7df654186bac440258b9be496cad4a
Nicolas FRAYSSE <nicolas1.fraysse@epitech.eu>
2018-12-03 19:29:12 +0100

    FUCKING BULLSHIT GIT

--- src/framework/exception.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "Framework/Framework.hpp"
-
-using namespace Framework;
-
-void FException::Log(FLogger &) const
-{
-    std::cerr << "Low-level Exception thrown : " << GetType() << std::endl;
-}
