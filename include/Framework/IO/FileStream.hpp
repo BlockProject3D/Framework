3673b2faaa14e2bf2c95edc8b3b2995f588dfacd
Nicolas FRAYSSE <nicolas1.fraysse@epitech.eu>
2018-12-19 14:48:23 +0100

    Added FileStream and IOException

---
+++ include/Framework/IO/FileStream.hpp
@@ -0,0 +1,100 @@
+// Copyright (c) 2018, BlockProject
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without modification,
+// are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright notice,
+//       this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright notice,
+//       this list of conditions and the following disclaimer in the documentation
+//       and/or other materials provided with the distribution.
+//     * Neither the name of BlockProject nor the names of its contributors
+//       may be used to endorse or promote products derived from this software
+//       without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#pragma once
+#include "Framework/IO/IInputStream.hpp"
+#include "Framework/IO/IOutputStream.hpp"
+#include "Framework/IO/File.hpp"
+
+namespace bpf
+{
+    constexpr int FILE_MODE_READ = 0x10;
+    constexpr int FILE_MODE_WRITE = 0x20;
+    constexpr int FILE_MODE_APPEND = 0x40;
+    constexpr int FILE_MODE_TRUNCATE = 0x80;
+
+    class BPF_API FileStream : public IInputStream, public IOutputStream
+    {
+    private:
+        int _mode;
+#ifdef WINDOWS
+        void *_handle;
+#else
+        int _handle;
+#endif
+    
+    public:
+        /**
+         * Creates a new FileStream from the given file and mode
+         * @param file file to open
+         * @param mode file mode ored (ex : FILE_MODE_READ | FILE_MODE_WRITE)
+         * @throws IOException
+         */
+        FileStream(const File &file, int mode);
+        ~FileStream();
+        
+        /**
+         * Cannot copy a FileStream
+         */
+        FileStream(const FileStream &other) = delete;
+        
+        /**
+         * Cannot copy a FileStream
+         */
+        FileStream operator=(const FileStream &other) = delete;
+        
+        /**
+         * Sets the file cursor position to an offset relative to the current position
+         * @param offset relative offset
+         * @throws IOException
+         */
+        void SeekOffset(int64 offset);
+        
+        /**
+         * Sets the file cursor position to pos
+         * @param pos new cursor position
+         * @throws IOException
+         */
+        void Seek(uint64 pos);
+        
+        /**
+         * Closes this FileStream, calling any more IO functions will throw IOException
+         */
+        void Close();
+        
+        /**
+         * @throws IOException
+         */
+        fsize Read(void *buf, fsize bufsize);
+
+        /**
+         * @throws IOException
+         */
+        fsize Write(const void *buf, fsize bufsize);
+    };
+}
---
+++ src/Framework/IO/FileStream.cpp
@@ -0,0 +1,127 @@
+// Copyright (c) 2018, BlockProject
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without modification,
+// are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright notice,
+//       this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright notice,
+//       this list of conditions and the following disclaimer in the documentation
+//       and/or other materials provided with the distribution.
+//     * Neither the name of BlockProject nor the names of its contributors
+//       may be used to endorse or promote products derived from this software
+//       without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#ifdef WINDOWS
+#else
+    #include <fcntl.h>
+    #include <unistd.h>
+    #include <errno.h>
+#endif
+#include "Framework/IO/FileStream.hpp"
+#include "Framework/IO/IOException.hpp"
+
+using namespace bpf;
+
+FileStream::FileStream(const File &file, int mode)
+    : _mode(mode)
+{
+#ifdef WINDOWS
+#else
+    int md = 0;
+    
+    if ((mode & FILE_MODE_READ) && (mode & FILE_MODE_WRITE))
+        md |= O_RDWR;
+    else if (mode & FILE_MODE_APPEND)
+        md |= O_APPEND;
+    else if (mode & FILE_MODE_READ)
+        md |= O_RDONLY;
+    else if (mode & FILE_MODE_WRITE)
+        md |= O_WRONLY;
+    if (mode & FILE_MODE_TRUNCATE)
+        md |= O_TRUNC;
+    _handle = open(*file.GetAbsolutePath().GetPath(), md, 0644);
+    if (_handle == -1)
+        throw IOException(String("Could not open file '")
+            + file.GetAbsolutePath().GetPath() + "' : "
+            + String(strerror(errno)));
+#endif
+}
+
+FileStream::~FileStream()
+{
+#ifdef WINDOWS
+#else
+    if (_handle != -1)
+        close(_handle);
+#endif
+}
+
+void FileStream::SeekOffset(int64 offset)
+{
+    if (_mode & FILE_MODE_APPEND)
+        throw IOException("Cannot Seek in append mode");
+#ifdef WINDOWS
+#else
+    if (_handle == -1)
+        throw IOException("File is closed");
+    lseek(_handle, offset, SEEK_CUR);
+#endif
+}
+
+void FileStream::Seek(uint64 pos)
+{
+    if (_mode & FILE_MODE_APPEND)
+        throw IOException("Cannot Seek in append mode");
+#ifdef WINDOWS
+#else
+    if (_handle == -1)
+        throw IOException("File is closed");
+    lseek(_handle, pos, SEEK_SET);
+#endif
+}
+
+void FileStream::Close()
+{
+    if (_handle == -1)
+        return;
+#ifdef WINDOWS
+#else
+    close(_handle);
+    _handle = -1;
+#endif
+}
+
+fsize FileStream::Read(void *buf, fsize bufsize)
+{
+    if (!(_mode & FILE_MODE_READ))
+        throw IOException("File has not been oppened with read mode");
+#ifdef WINDOWS
+#else
+    return (read(_handle, buf, bufsize));
+#endif
+}
+
+fsize FileStream::Write(const void *buf, fsize bufsize)
+{
+    if (!(_mode & FILE_MODE_WRITE))
+        throw IOException("File has not been oppened with write mode");
+#ifdef WINDOWS
+#else
+    return (write(_handle, buf, bufsize));
+#endif
+}
