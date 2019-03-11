22dc7c95bb1ec199c6fb163201628520f3b9036b
Nicolas FRAYSSE <nicolas1.fraysse@epitech.eu>
2019-03-11 01:50:18 +0100

    Added missing Types include to Map

--- include/Framework/Map.hpp
+++ include/Framework/Map.hpp
@@ -27,6 +27,7 @@
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #pragma once
+#include "Framework/Types.hpp"
 #include "Framework/Iterator.hpp"
 #include "Framework/IndexException.hpp"
 
