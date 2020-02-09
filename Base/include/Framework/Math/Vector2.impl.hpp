26f99bd994154f375ae660ce124a72e4068054b9
Yuri Edward <nicolas1.fraysse@epitech.eu>
2020-02-09 20:38:57 +0100

    Rewrite Vector2

---
+++ Base/include/Framework/Math/Vector2.impl.hpp
@@ -0,0 +1,79 @@
+// Copyright (c) 2020, BlockProject
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without modification,
+// are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright notice,
+//       this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright notice,
+//       this list of conditions and the following disclaimer in the documentation
+//       and/or other materials provided with the distribution.
+//     * Neither the name of BlockProject nor the names of its contributors
+//       may be used to endorse or promote products derived from this software
+//       without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#pragma once
+
+namespace bpf
+{
+    template <typename T>
+    Vector<T, 2>::Vector(const std::initializer_list<T> &lst)
+    {
+        fsize i = 0;
+
+        for (auto &elem : lst)
+        {
+            switch (i)
+            {
+            case 0:
+                X = elem;
+                break;
+            case 1:
+                Y = elem;
+                break;
+            }
+        }
+    }
+
+    template <typename T>
+    T &Vector<T, 2>::operator()(const fsize l)
+    {
+        switch (l)
+        {
+        case 0:
+            return (X);
+        case 1:
+            return (Y);
+        default:
+            throw IndexException((fisize)l);
+        }
+    }
+
+    template <typename T>
+    const T &Vector<T, 2>::operator()(const fsize l) const
+    {
+        switch (l)
+        {
+        case 0:
+            return (X);
+        case 1:
+            return (Y);
+        default:
+            throw IndexException((fisize)l);
+        }
+    }
+}
