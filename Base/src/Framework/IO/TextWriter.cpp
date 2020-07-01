e1f4f91dd7c165f40bebd1584d2c882a2152695c
Yuri Edward <nicolas1.fraysse@epitech.eu>
2020-07-01 02:44:50 +0200

    Fixed bug with Flush which caused rethrow on the same data

--- Base/src/Framework/IO/TextWriter.cpp
+++ Base/src/Framework/IO/TextWriter.cpp
@@ -4,7 +4,7 @@
 //
 // Redistribution and use in source and binary forms, with or without modification,
 // are permitted provided that the following conditions are met:
-// 
+//
 //     * Redistributions of source code must retain the above copyright notice,
 //       this list of conditions and the following disclaimer.
 //     * Redistributions in binary form must reproduce the above copyright notice,
@@ -109,8 +109,10 @@ void TextWriter::Flush()
 {
     if (_buffered)
     {
-        _stream.Write(*_buf, _buf.GetWrittenBytes());
+        // Inverse logic to avoid re-throwing the same exception
+        auto size = _buf.GetWrittenBytes();
         _buf.Reset();
+        _stream.Write(*_buf, size);
     }
 }
 
