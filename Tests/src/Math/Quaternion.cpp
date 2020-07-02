1a5f49a239d5fa7f80a1f173706fb2ef59e249fb
Yuri Edward <nicolas1.fraysse@epitech.eu>
2020-07-02 18:24:17 +0200

    Added test for negative dot product

--- Tests/src/Math/Quaternion.cpp
+++ Tests/src/Math/Quaternion.cpp
@@ -110,7 +110,7 @@ TEST(Quat, Interpolation)
 {
     bpf::Quatld q = bpf::Quatld(bpf::Vector3ld(bpf::math::Mathld::Pi / 2.0, bpf::math::Mathld::Pi / 2.0, bpf::math::Mathld::Pi / 4.0));
     bpf::Quatld q1 = bpf::Quatld(bpf::Vector3ld(1, 0, 0), bpf::math::Mathld::Pi / 2.0);
-    bpf::Quatld q2 = bpf::Quatld(bpf::Vector3ld(1, 0, 0), -bpf::math::Mathld::Pi / 2.0);
+    bpf::Quatld q2 = bpf::Quatld(bpf::Vector3ld(-1, -1, 1), bpf::math::Mathld::Pi / 2.0);
 
     EXPECT_EQ(bpf::Quatld::Lerp(q, q, 1.0f), q);
     EXPECT_EQ(bpf::Quatld::Lerp(q, q, 0.1f), q);
