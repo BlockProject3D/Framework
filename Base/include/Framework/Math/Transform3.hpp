81a5d3164ae48ab287c14edc5596d762740196f0
Yuri Edward <yuri6037@outlook.com>
2020-07-10 16:28:18 +0200

    Update doc for 3D and 2D transforms

--- Base/include/Framework/Math/Transform3.hpp
+++ Base/include/Framework/Math/Transform3.hpp
@@ -35,31 +35,89 @@ namespace bpf
 {
     namespace math
     {
+        /**
+         * Utility to represent a transformation in 3D space
+         * @tparam T the type of number
+         */
         template <typename T>
         class BP_TPL_API Transform3
         {
         public:
+            /**
+             * Utility to build matrices for 3D transformations in homogeneous coordinate space
+             */
             class BP_TPL_API MatrixBuilder
             {
             private:
                 Matrix4<T> _matrix;
 
             public:
+                /**
+                 * Constructs a MatrixBuilder from identity transform
+                 */
                 inline MatrixBuilder()
                     : _matrix(Matrix4<T>::Identity)
                 {
                 }
+
+                /**
+                 * Push a new translation to the current matrix
+                 * @param translation new translation vector
+                 * @return reference for chaining
+                 */
                 MatrixBuilder &Translate(const Vector3<T> &translation) noexcept;
+
+                /**
+                 * Push a new scale to the current matrix
+                 * @param scale new scale vector
+                 * @return reference for chaining
+                 */
                 MatrixBuilder &Scale(const Vector3<T> &scale) noexcept;
+
+                /**
+                 * Push a new shear to the current matrix
+                 * @param shear shear value
+                 * @return reference for chaining
+                 */
                 MatrixBuilder &ShearX(const Vector3<T> &shear) noexcept;
+
+                /**
+                 * Push a new shear to the current matrix
+                 * @param shear shear value
+                 * @return reference for chaining
+                 */
                 MatrixBuilder &ShearY(const Vector3<T> &shear) noexcept;
+
+                /**
+                 * Push a new shear to the current matrix
+                 * @param shear shear value
+                 * @return reference for chaining
+                 */
                 MatrixBuilder &ShearZ(const Vector3<T> &shear) noexcept;
+
+                /**
+                 * Push a new rotation to the current matrix
+                 * @param axis rotation axis
+                 * @param angle angle in radians
+                 * @return reference for chaining
+                 */
                 MatrixBuilder &Rotate(const Vector3<T> &axis, T angle) noexcept;
+
+                /**
+                 * Push a new rotation to the current matrix
+                 * @param quat unit quaternion
+                 * @return reference for chaining
+                 */
                 inline MatrixBuilder &Rotate(const Quaternion<T> &quat) noexcept
                 {
                     _matrix = _matrix * quat.ToMatrix();
                     return (*this);
                 }
+
+                /**
+                 * Returns the built matrix
+                 * @return order 4 square matrix
+                 */
                 inline Matrix4<T> Build() const noexcept
                 {
                     return (_matrix);
@@ -67,10 +125,27 @@ namespace bpf
             };
 
         public:
+            /**
+             * Transform position
+             */
             Vector3<T> Position;
+
+            /**
+             * Transform scale
+             */
             Vector3<T> Scale;
+
+            /**
+             * Transform rotation as a unit quaternion
+             */
             Quaternion<T> Rotation;
 
+            /**
+             * Constructs a 3D transform
+             * @param pos transform position
+             * @param scale transform scale
+             * @param rotation transform rotation
+             */
             explicit inline Transform3(const Vector3<T> &pos = Vector3<T>::Zero, const Vector3<T> &scale = Vector3<T>::Identity, const Quaternion<T> &rotation = Quaternion<T>::Identity)
                 : Position(pos)
                 , Scale(scale)
@@ -78,19 +153,55 @@ namespace bpf
             {
             }
 
+            /**
+             * Transforms a point
+             * @param local 3D point to transform
+             * @return new transformed 3D point
+             */
             Vector3<T> LocalToWorld(const Vector3<T> &local);
+
+            /**
+             * Revert transformation from a point
+             * @param world 3D point to revert transformation
+             * @return new de-transformed 3D point
+             */
             Vector3<T> WorldToLocal(const Vector3<T> &world);
 
+            /**
+             * Computes the matrix of this transform
+             * @return order 4 square matrix
+             */
             inline Matrix4<T> ToMatrix() const noexcept
             {
                 return (MatrixBuilder().Translate(Position).Rotate(Rotation).Scale(Scale).Build());
             }
 
+            /**
+             * Adds two transforms
+             * @param other transform to add
+             * @return new transform
+             */
             Transform3 operator+(const Transform3 &other) const noexcept;
+
+            /**
+             * Adds a transform to this transform
+             * @param other transform to add
+             */
             void operator+=(const Transform3 &other);
 
+            /**
+             * Rotates this transform around a point
+             * @param pivot rotation origin
+             * @param rotation unit quaternion
+             */
             void RotateArround(const Vector3<T> &pivot, const Quaternion<T> &rotation) noexcept;
 
+            /**
+             * Updates this transform to look at a given point
+             * @param target the target point to look at
+             * @param forward the world's forward vector
+             * @param up the world's up vector
+             */
             inline void LookAt(const Vector3<T> &target, const Vector3<T> &forward = Vector3<T>::Forward, const Vector3<T> &up = Vector3<T>::Up) noexcept
             {
                 Rotation = Quaternion<T>::LookAt(target - Position, forward, up);
