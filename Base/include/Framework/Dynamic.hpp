517d6ac4e46046ada3c3717249f5069cf4044d3b
Yuri Edward <yuri6037@outlook.com>
2020-07-18 23:47:24 +0200

    Initial (untested) implementation for bpf::Dynamic

--- Base/include/Framework/Dynamic.hpp
+++ Base/include/Framework/Dynamic.hpp
@@ -27,8 +27,10 @@
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #pragma once
+#include <utility>
 #include "Framework/TypeInfo.hpp"
 #include "Framework/Memory/MemUtils.hpp"
+#include "Framework/Memory/ClassCastException.hpp"
 
 namespace bpf
 {
@@ -38,29 +40,156 @@ namespace bpf
     class BPF_API Dynamic
     {
     private:
-        void *_data;
-        fsize _typeid;
+        class Storage
+        {
+        public:
+            fsize TypeId;
+            void *DataPtr;
+            explicit inline Storage(const fsize type, void *dptr)
+                : TypeId(type)
+                , DataPtr(dptr)
+            {
+            }
+            virtual ~Storage() {}
+            virtual Storage *Clone() = 0;
+            virtual const char *GetTypeName() const noexcept = 0;
+        };
+
+        template <typename T>
+        class DynamicStorage final : public Storage, T
+        {
+        private:
+            const char *_tname;
+
+        public:
+            explicit inline DynamicStorage(const T &other)
+                : Storage(TypeIndex<T>(), static_cast<T *>(this))
+                , T(other)
+                , _tname(TypeName<T>())
+            {
+            }
+
+            inline const char *GetTypeName() const noexcept final
+            {
+                return (_tname);
+            }
+
+            explicit inline DynamicStorage(T &&other)
+                : Storage(TypeIndex<T>(), static_cast<T *>(this))
+                , T(std::move(other))
+                , _tname(TypeName<T>())
+            {
+            }
+
+            inline Storage *Clone() final
+            {
+                return (memory::MemUtils::New<T>(*this));
+            }
+        };
+
+        Storage *_storage;
+
+        explicit inline Dynamic(Storage *storage)
+            : _storage(storage)
+        {
+        }
 
     public:
-        Dynamic();
-        template <typename T>
-        Dynamic(const T &other);
-        template <typename T>
-        Dynamic(T &&other);
-        Dynamic(const Dynamic &other);
-        Dynamic(Dynamic &&other) noexcept;
+        inline Dynamic()
+            : _storage(Null)
+        {
+        }
 
-        ~Dynamic();
+        inline Dynamic(const Dynamic &other)
+            : _storage(other._storage->Clone())
+        {
+        }
 
-        Dynamic &operator=(const Dynamic &other);
-        Dynamic &operator=(Dynamic &&other) noexcept;
+        inline Dynamic(Dynamic &&other) noexcept
+            : _storage(other._storage)
+        {
+            other._storage = Null;
+        }
+
         template <typename T>
-        Dynamic &operator=(const T &other);
+        inline Dynamic(const T &other, typename std::enable_if<!std::is_same<T, Dynamic>::value>::type* = 0)
+            : _storage(memory::MemUtils::New<DynamicStorage<T>>(other))
+        {
+        }
+
         template <typename T>
-        Dynamic &operator=(T &&other);
+        inline Dynamic(T &&other, typename std::enable_if<!std::is_same<T, Dynamic>::value>::type* = 0)
+            : _storage(memory::MemUtils::New<DynamicStorage<T>>(std::forward<T>(other)))
+        {
+        }
+
+        inline ~Dynamic()
+        {
+            memory::MemUtils::Delete(_storage);
+        }
+
+        inline fsize TypeId() const noexcept
+        {
+            return (_storage == Null ? 0 : _storage->TypeId);
+        }
+
+        inline Dynamic &operator=(const Dynamic &other)
+        {
+            if (this == &other)
+                return (*this);
+            memory::MemUtils::Delete(_storage);
+            _storage = other._storage == Null ? Null : other._storage->Clone();
+            return (*this);
+        }
+
+        inline Dynamic &operator=(Dynamic &&other) noexcept
+        {
+            if (this == &other)
+                return (*this);
+            memory::MemUtils::Delete(_storage);
+            _storage = other._storage;
+            other._storage = Null;
+            return (*this);
+        }
+
         template <typename T>
-        operator T &();
+        inline Dynamic &operator=(const T &other)
+        {
+            if (this == &other)
+                return (*this);
+            memory::MemUtils::Delete(_storage);
+            _storage = memory::MemUtils::New<DynamicStorage<T>>(other);
+            return (*this);
+        }
+
         template <typename T>
-        operator const T &() const;
+        inline Dynamic &operator=(T &&other)
+        {
+            if (this == &other)
+                return (*this);
+            memory::MemUtils::Delete(_storage);
+            _storage = memory::MemUtils::New<DynamicStorage<T>>(std::forward<T>(other));
+            return (*this);
+        }
+
+        template <typename T>
+        inline operator T &()
+        {
+            if (_storage == Null)
+                throw memory::ClassCastException("Cannot cast null object");
+            if (TypeIndex<T>() != TypeId())
+                throw memory::ClassCastException(String("Cannot cast from ") + _storage->GetTypeName() + " to " + TypeName<T>());
+            return (*reinterpret_cast<T *>(_storage->DataPtr));
+        }
+
+        template <typename T>
+        inline operator const T &() const
+        {
+            if (_storage == Null)
+                throw memory::ClassCastException("Cannot cast null object");
+            if (TypeIndex<T>() != TypeId())
+                throw memory::ClassCastException(String("Cannot cast from ") + _storage->GetTypeName() + " to " + TypeName<T>());
+            return (*reinterpret_cast<T *>(_storage->DataPtr));
+        }
     };
 }
