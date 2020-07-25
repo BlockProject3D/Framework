65adef8e8a7febb562080dbeb2de5f16f93b6589
Yuri Edward <nicolas1.fraysse@epitech.eu>
2020-07-25 20:30:56 +0200

    Fixed FileStream test under Windows

--- Tests/src/IO/FileStream.cpp
+++ Tests/src/IO/FileStream.cpp
@@ -59,8 +59,8 @@ TEST(FileStream, Move)
 {
     bpf::io::File f("./doesnotexist.txt");
     bpf::io::FileStream stream(f, bpf::io::FILE_MODE_WRITE | bpf::io::FILE_MODE_TRUNCATE);
-
     auto stream1 = std::move(stream);
+    stream1.Close();
     stream1 = bpf::io::FileStream(f, bpf::io::FILE_MODE_WRITE | bpf::io::FILE_MODE_TRUNCATE);
     stream = std::move(stream1);
     EXPECT_EQ(stream.Write("This is a test", 14), (bpf::fsize)14);
