9c6dc23301310e0c60bfed225e276930a3388eaf
Yuri Edward <yuri6037@outlook.com>
2020-08-26 10:47:50 +0200

    Added missing TypeInfo test file

---
+++ Tests/src/TypeInfo.cpp
@@ -0,0 +1,46 @@
+// Copyright (c) 2020, BlockProject 3D
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without modification,
+// are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright notice,
+//       this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright notice,
+//       this list of conditions and the following disclaimer in the documentation
+//       and/or other materials provided with the distribution.
+//     * Neither the name of BlockProject 3D nor the names of its contributors
+//       may be used to endorse or promote products derived from this software
+//       without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#include <Framework/TypeInfo.hpp>
+#include <gtest/gtest.h>
+
+TEST(TypeInfo, Base)
+{
+    EXPECT_STREQ(bpf::TypeName<int>(), "bpf::int32");
+    EXPECT_STREQ(bpf::TypeName<float>(), "float");
+    EXPECT_STREQ(bpf::TypeName<double>(), "double");
+    EXPECT_STREQ(bpf::TypeName<bool>(), "bool");
+    EXPECT_STREQ(bpf::TypeName<bpf::int32>(), "bpf::int32");
+    EXPECT_STREQ(bpf::TypeName<bpf::fint>(), "bpf::int32");
+    EXPECT_STREQ(bpf::TypeName<bpf::int64>(), "bpf::int64");
+    EXPECT_STREQ(bpf::TypeName<bpf::uint64>(), "bpf::uint64");
+    EXPECT_STREQ(bpf::TypeName<bpf::int16>(), "bpf::int16");
+    EXPECT_STREQ(bpf::TypeName<bpf::uint16>(), "bpf::uint16");
+    EXPECT_STREQ(bpf::TypeName<bpf::int8>(), "bpf::int8");
+    EXPECT_STREQ(bpf::TypeName<bpf::uint8>(), "bpf::uint8");
+}
